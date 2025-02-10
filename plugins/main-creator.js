71421dd547c01832aefbae68884c74b1b38d4a71
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-10 14:16:32 -0300

    𝑷𝑶𝑾𝑬𝑹 𝑩𝒀 𝑹𝑨𝒀𝑶
    UNA VEZ MAS ROMPIEDOLA

--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙊𝙣𝙮𝙭;;\nFN:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙊𝙣𝙮𝙭\nORG:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙊𝙣𝙮𝙭\nTITLE:\nitem1.TEL;waid=593985327644:593985327644\nitem1.X-ABLabel:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙊𝙣𝙮𝙭\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙊𝙣𝙮𝙭\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
